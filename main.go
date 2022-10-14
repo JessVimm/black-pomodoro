ac41267c8b12f102709c522e2cec9a38eec9efe3
Jess Vimm <jessvimm@gmail.com>
2022-10-14 01:27:09 -0500

    Features added to draft

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 var numTasks int
@@ -52,20 +53,97 @@ func getData() {
 
 		totalPomodoros = append(totalPomodoros, pomodoroLen)
 		totalTasks = append(totalTasks, currentTask)
+	}
 
-		fmt.Println(totalTasks)
-		fmt.Println(totalPomodoros)
+	userReady := ""
+
+	fmt.Print("Ready? (Y/n) -> ")
+	fmt.Scan(&userReady)
+	if userReady == "Y" || userReady == "y" || userReady == "Yes" || userReady == "yes" {
+		pomodoros(totalPomodoros, totalTasks)
+	} else {
+		fmt.Println("All right! See you next time :)")
+		os.Exit(1)
+	}
+	fmt.Println("All done! You did a great job :D")
+	fmt.Println("See you soon!")
+
+}
+
+func pomodoros(pomodoros []int, tasks []Tasks) {
+	thisPTaskMinutes := 0
+	done := make(chan bool) // Channel
+	thisPTaskDone := 0
+	totalPomodoros := 0
+	currPCount := 0
+
+	// Calculate total Pomodoros for this session
+	for n := 0; n < len(pomodoros); n++ {
+		totalPomodoros += tasks[n].numPomodoros
+	}
+
+	// Get into each Pomodoro
+	if len(tasks) > 0 {
+		for index := range tasks {
+			thisPTaskMinutes = pomodoros[index] // Each pomodoro time for current task
+			for pAmount := tasks[index].numPomodoros; pAmount > 0; pAmount-- {
+				// Begin countdown
+				fmt.Printf("Beginning with pomodoro # %v from task %s", pAmount+1, tasks[index].taskName)
+				fmt.Println()
+				countdown(thisPTaskMinutes)
+				done <- true
+				<-done
+				// Update pomodoros done for this task
+				thisPTaskDone++
+				// Update total pomodoros done
+				currPCount++
+
+				// Check for breaks
+				checkForBreaks(thisPTaskDone, tasks[index].numPomodoros, currPCount, totalPomodoros, thisPTaskMinutes)
+			}
+			thisPTaskDone = 0
+		}
+	} else {
+		fmt.Println("Ups o.o seems like you didn't enter any tasks for today. Please, try again :)")
+		os.Exit(1)
 	}
 }
 
-// func pomodoros() {
-// 	return
+func countdown(minutes int) {
+	// Start countdown
+	// ring
+}
+
+func checkForBreaks(pTaskDone int, taskPomodoros int, currPCount int, totalP int, pMinutes int) {
+	// Have we finished the entire session? currPCount == totalP Yes? Then...
+	// No need for breaks, say goodbye!
+
+	// Have we finished this task pomodoros? pTaskDone == taskPomodoros Yes? then...
+	// Apply this task long break
+	// applyLongBreak(pMinutes int)
+
+	// Have we gone through half of this taks pomodoros? pTaskDone == (taskPomodoros/2) Yes? then...
+	// Apply long break
+	// applyLongBreak(pMinutes int)
+
+	// Else, regular break for this task
+	// applyRegularBreak(pMinutes int)
+
+}
+
+// func applyRegularBreak(pMinutes int) {
+// Calculate break time: breakTime = pMinutes * 0.20
+// Apply break countdown(breakTime)
+// ring
 // }
 
-// func checkForBreaks() {
-// 	return
+// func applyLongBreak(pMinutes int) {
+// Calculate break time: if pMinutes > 25 then breakTime = pMinutes*0.8, else if pMinutes <= 25 then breakTime = pMinutes*0.6
+// Apply break countdown(breakTime)
+// ring
 // }
 
-// func applyBreak() {
-// 	return
+// func ring() {
+// Do the ring
+// Wait for user
 // }
